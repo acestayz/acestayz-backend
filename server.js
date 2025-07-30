cbf4ff5652761e37ee6b92fb5c88e748998b8b0f
acestayz <itacestayz@gmail.com>
2025-07-30 13:22:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -29,5 +28,4 @@ mongoose
     process.exit(1); // Optional: stop app if DB doesn't connect
   });
 
-// Routes
 app.use("/api/contact", contactRoutes);
